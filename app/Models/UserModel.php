dd01f0fb2ef9ad1c111865aa90a29dbaced7c104
William Alonzo <williamalonzo4519@gmail.com>
2025-01-18 00:22:49 -0600

    Modelo usuario

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -8,7 +8,7 @@ class UserModel extends Model
 {
     protected $table = 'users';
     protected $primaryKey = 'id';
-    protected $allowedFields = ['email', 'password', 'role', 'name', 'dpi', 'group_id', 'pos', 'estado'];
+    protected $allowedFields = ['email', 'password', 'role', 'name', 'dpi', 'group_id', 'pos', 'estado', 'territorio'];
     protected $returnType = 'array';
     protected $useTimestamps = true;
 }
